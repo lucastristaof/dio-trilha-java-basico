5c17726e47663545b0f9bf27adaa3055cb76ac2e
lucastristaof <lucas.tristaof@gmail.com>
2023-04-14 14:54:51 -0300

    Tipos de variáveis e declaração de constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 123;
+        short ano = 2635;
+        int cep = 27092110;
+        String cepComZero = "07092110";
+        long cpf = 41938769858L;
+        float pi = 3.14f;
+        double salarioMinimo = 2500;
+        
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
