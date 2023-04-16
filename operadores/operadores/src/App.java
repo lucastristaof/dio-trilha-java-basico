36860c11c7ea94894f0c32202bd24e71e03b2b26
lucastristaof <lucas.tristaof@gmail.com>
2023-04-16 13:45:48 -0300

    Operadores aritméticos

---
+++ operadores/operadores/src/App.java
@@ -0,0 +1,29 @@
+import java.sql.Date;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //atribuição, cada tipo de dado tem uma forma de atribuição
+        String nome = "Lucas";
+        int idade = 22;
+        double peso = 62.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+        
+        
+        //Aritméticos
+
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+
+        //Adição em String é concatenação
+
+        String nomeCompleto = "Lucas " + "Tristão";
+        
+
+    }
+}
